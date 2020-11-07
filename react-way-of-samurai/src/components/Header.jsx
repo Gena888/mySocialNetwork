c04335ae7753df0bf019882fc0ee8a434630df2c
Gena888 <theGena888@gmail.com>
2020-11-07 08:03:36 +0300

    add react

---
+++ react-way-of-samurai/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://sun1.beltelecom-by-minsk.userapi.com/impf/c847220/v847220418/c6148/DDmHnFk6Uvw.jpg?size=811x959&quality=96&proxy=1&sign=ee3b4d32afdbd5e8cff1545a788a68bd" alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ react-way-of-samurai/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://v-georgia-com.exactdn.com/wp-content/uploads/2017/08/%D0%9F%D0%BB%D1%8F%D0%B6%D0%91%D0%B0%D1%82%D1%83%D0%BC%D0%B86%D0%97.jpg?lossy=1&quality=60&ssl=1" alt="" />
+            </div>
+            <div>
+                ava + discp
+        </div>
+            <div>
+                my posts
+          <div>
+                    new post
+          </div>
+            </div>
+            <div>
+                post 1
+        </div>
+            <div>
+                post 2
+        </div>
+
+        </div>
+    );
+}
+
+export default Profile;
