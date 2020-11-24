0a4f9172fe25cc9ba26d1e56c8065219ed019eb9
Gena888 <theGena888@gmail.com>
2020-11-24 08:05:04 +0300

    52 done refactored a little

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,33 +5,29 @@ import userPhoto from '../../imagas/no-avatar.png'
 
 
 const Users = (props) => {
-    // debugger
-
-    // if (props.usersData.length === 0) {
-    //     props.setUsers([
-    //         { id: 1, followed: false, name: 'Gena', Status: 'Learning now', location: { city: 'Minsk', country: 'Belarus' }, photoUrl: 'https://sun1.beltelecom-by-minsk.userapi.com/c847220/v847220418/c6148/DDmHnFk6Uvw.jpg' },
-    //         { id: 2, followed: true, name: 'Gleb', Status: 'Seating now', location: { city: 'Moskow', country: 'Russia' }, photoUrl: 'https://sun9-18.userapi.com/cqGGLrpxAgHQf0x5w9-mtcIKXXXNyF50BocQyA/eAjdupisXrU.jp' },
-    //         { id: 3, followed: true, name: 'Igor', Status: 'in de house', location: { city: 'London', country: 'Uk' }, photoUrl: 'https://sun1.beltelecom-by-minsk.userapi.com/c844417/v844417074/6d041/N75krJg5dtc.jpg' },
-    //         { id: 4, followed: false, name: 'Sasha', Status: 'playing', location: { city: 'Borisov', country: 'Belarus' }, photoUrl: 'https://sun1.beltelecom-by-minsk.userapi.com/c627716/v627716457/2f13c/k4JKIT907iM.jpg' }
-    //     ])
-    // }
 
 
 
-    if (props.usersData.length === 0) {
-        debugger
+    let getUsers = () => {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                debugger;
-                props.setUsers(response.data.items)
-            });
+        if (props.usersData.length === 0) {
+            debugger
+
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    debugger;
+                    props.setUsers(response.data.items)
+                });
+        }
+
     }
 
 
-    return <div>
-        {
-            props.usersData.map((u) => <div key={u.id}>
+
+    return (
+        <div>
+            <button onClick={getUsers} >getUsers</button>
+            {props.usersData.map((u) => <div key={u.id}>
                 <span>
                     <div>
                         <img className={s.userPhoto}
@@ -55,8 +51,8 @@ const Users = (props) => {
                     </span>
                 </span>
             </div>)
-        }
-    </div>
+            }
+        </div>)
 };
 
 export default Users;
