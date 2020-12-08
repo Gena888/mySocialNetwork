b1bcc8efe4d2b136789ad05dbb7399c29bd4875e
Gena888 <theGena888@gmail.com>
2020-12-08 16:58:15 +0300

    refactored myPosts. changed class component by functional component. added React.memo. the same logic as in pure component. now react will not render jsx if state or props gote no changes.

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,41 +1,34 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Post from './Post/Post';
 import s from './MyPosts.module.css';
 import { Field, reduxForm, reset } from 'redux-form';
 import { maxLengthCreator, required } from './../../../Utils/Validators/Validaors';
 import { Textarea } from './../../Common/FormsControls/FormsControls';
-import { render } from '@testing-library/react';
 
 
 
-class MyPosts extends PureComponent {
+const MyPosts = React.memo((props) => {
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return nextProps != this.props || nextState != this.state
-    // } ЭТА ЛОГИКА СКРЫТА В PURE COMPONENT 
+    let postsElement = props.postsData.map(postEl => <Post key={postEl.id} likes={postEl.likes} message={postEl.message} />)
 
-    render() {
-
-        let postsElement = this.props.postsData.map(postEl => <Post key={postEl.id} likes={postEl.likes} message={postEl.message} />)
-
-        let onAddNewPost = (values) => {
-            this.props.addPost(values.newTextBody)
-        }
-
-        return (
-            <div className={s.postsBlock} >
-                <h3> my posts</h3>
-
-                <ReduxAddPost onSubmit={onAddNewPost} />
-
-                {postsElement}
-                <div className={s.posts}>
-
-                </div>
-            </div>
-        );
+    let onAddNewPost = (values) => {
+        props.addPost(values.newTextBody)
     }
-}
+
+    return (
+        <div className={s.postsBlock} >
+            <h3> my posts</h3>
+
+            <ReduxAddPost onSubmit={onAddNewPost} />
+
+            {postsElement}
+            <div className={s.posts}>
+
+            </div>
+        </div>
+    );
+
+})
 
 const maxLength = maxLengthCreator(10);
 
