a18a31f4fd53704b2ed49d8ccf4173f4df3fc2ce
Gena888 <theGena888@gmail.com>
2020-12-10 22:32:59 +0300

    added hoc with lazy loaging ti remove login and music components from main chunk

--- src/App.js
+++ src/App.js
@@ -8,47 +8,48 @@ import NavContainer from './components/Nav/NavContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './Login/Login';
 import { connect, Provider } from 'react-redux';
 import Preloader from './components/Common/Preloader/Preloader';
 import { initializeApp } from './redux/app-reducer';
 import { compose } from 'redux';
 import store from './redux/redux-store';
+import { withSuspense } from './Hoc/withSuspense';
+
 const Music = React.lazy(() => import('./components/Music/Music'))
-const Music = React.lazy(() => import('./Login/Login''))
+const Login = React.lazy(() => import('./Login/Login'))
 
 
 class App extends Component {
 
-    componentDidMount() {
-      this.props.initializeApp()
-    }
-
-    render() {
-
-      if (!this.props.initialized) {
-        return <Preloader />
-      }
-
-      return (
-        <div className="app-wrapper">
-
-          <HeaderContainer />
-          <NavContainer />
-          <div className='app-wrapper-content'>
-            <Route path='/Profile/:userId?' render={() => <ProfileContainer />} />
-            <Route path='/Dialogs' render={() => <DialogsContainer />} />
-            <Route path='/Users' render={() => <UsersContainer />} />
-            <Route path='/News' render={() => <News />} />
-            <Route path='/Music' render={() => { return <React.Suspense fallback={<Preloader />}> <Music /></React.Suspense> }} />
-            <Route path='/Settings' render={() => <Settings />} />
-            <Route path='/Login' render={() => { return <React.Suspense fallback={<Preloader />}> <Login /> </React.Suspense> }} />
-          </div>
-
-        </div>
-      );
-    }
+  componentDidMount() {
+    this.props.initializeApp()
   }
+
+  render() {
+
+    if (!this.props.initialized) {
+      return <Preloader />
+    }
+
+    return (
+      <div className="app-wrapper">
+
+        <HeaderContainer />
+        <NavContainer />
+        <div className='app-wrapper-content'>
+          <Route path='/Profile/:userId?' render={() => <ProfileContainer />} />
+          <Route path='/Dialogs' render={() => <DialogsContainer />} />
+          <Route path='/Users' render={() => <UsersContainer />} />
+          <Route path='/News' render={() => <News />} />
+          <Route path='/Music' render={withSuspense(Music)} />
+          <Route path='/Settings' render={() => <Settings />} />
+          <Route path='/Login' render={() => { return <React.Suspense fallback={<Preloader />}> <Login /> </React.Suspense> }} />
+        </div>
+
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized
