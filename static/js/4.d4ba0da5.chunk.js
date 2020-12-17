8500b570389e52637b2a92dbdbff0f64e48077e8
Gena888 <theGena888@gmail.com>
2020-12-17 22:32:11 +0300

    Updates

---
+++ static/js/4.d4ba0da5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-kabzda-1"]=this["webpackJsonpreact-kabzda-1"]||[]).push([[4],{305:function(s,a,e){s.exports={class:"News_class__23xtr"}},307:function(s,a,e){"use strict";e.r(a);var c=e(0),t=(e(1),e(305)),n=e.n(t);a.default=function(s){return Object(c.jsx)("div",{className:n.a.class,children:"Here will be news"})}}}]);
+//# sourceMappingURL=4.d4ba0da5.chunk.js.map
